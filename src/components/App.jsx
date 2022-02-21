dbe39a3728feeaf4fb13ba0430ebdafa414b10c1
Travis Frank <travis.frank@placeexchange.com>
2022-02-21 13:57:15 -0500

    feat: Landing component init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import Landing from './landing/Landing';
 import NavigationButton from './navigation/NavigationButton';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <div className="App">
+      <Landing />
       <NavigationButton />
     </div>
   )
---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,11 @@
+import './Landing.css';
+
+function Landing() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Landing;
