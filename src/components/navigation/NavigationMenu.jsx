bf685d0cf7cd50f57ef7f8c13d572ef062a0a37f
Travis Frank <travis.frank@placeexchange.com>
2022-02-22 22:52:22 -0500

    feat: NavSubmenu animations

--- src/components/navigation/NavigationMenu.jsx
+++ src/components/navigation/NavigationMenu.jsx
@@ -3,12 +3,12 @@ import { useState } from 'react';
 import {
   GiBookshelf,
   GiCandlestickPhone,
-  GiCheckboxTree,
   GiDirectorChair,
   GiEarthAmerica,
   GiGrandPiano,
   GiHeadphones,
   GiHobbitDoor,
+  GiMusicSpell,
   GiPencil,
   GiProcessor,
   GiSaxophone,
@@ -18,12 +18,13 @@ import {
   GiSkills,
   GiTheaterCurtains
 } from 'react-icons/gi'
+import { CSSTransition } from 'react-transition-group';
 import './NavigationMenu.css';
 import NavigationSubmenu from './NavigationSubmenu';
 import NavigationItem from './NavigationItem';
 
 function NavigationMenu({ open }) {
-  const [openSubmenu, setOpenSubmenu] = useState('about');
+  const [openSubmenu, setOpenSubmenu] = useState('');
 
   const navMenuClass = classNames({
     open: open,
@@ -51,42 +52,51 @@ function NavigationMenu({ open }) {
         text={"About"}
         Icon={GiSpaceNeedle}
       />
-      {
-        openSubmenu === 'about' &&
+      <CSSTransition
+        in={openSubmenu === 'about'}
+        timeout={750}
+        unmountOnExit
+      >
         <NavigationSubmenu>
           <NavigationItem text={"Bio"} Icon={GiSkills} />
           <NavigationItem text={"Principles"} Icon={GiScales} />
           <NavigationItem text={"Leadership"} Icon={GiDirectorChair} />
         </NavigationSubmenu>
-      }
+      </CSSTransition>
       <NavigationItem
         onClick={createClickHandler('writing')}
         text={"Writing"}
         Icon={GiBookshelf}
       />
-      {
-        openSubmenu === 'writing' &&
+      <CSSTransition
+        in={openSubmenu === 'writing'}
+        timeout={750}
+        unmountOnExit
+      >
         <NavigationSubmenu>
           <NavigationItem text={"Words"} Icon={GiPencil} />
           <NavigationItem text={"Musicals"} Icon={GiTheaterCurtains} />
           <NavigationItem text={"Jazz"} Icon={GiSaxophone} />
         </NavigationSubmenu>
-      }
+      </CSSTransition>
       <NavigationItem text={"Open Source"} Icon={GiProcessor} />
       <NavigationItem
         onClick={createClickHandler('music')}
         text={"Music"}
         Icon={GiSoundWaves}
       />
-      {
-        openSubmenu === 'music' &&
+      <CSSTransition
+        in={openSubmenu === 'music'}
+        timeout={750}
+        unmountOnExit
+      >
         <NavigationSubmenu>
           <NavigationItem text={"Musicals"} Icon={GiTheaterCurtains} />
           <NavigationItem text={"Live Reels"} Icon={GiGrandPiano} />
           <NavigationItem text={"Recordings"} Icon={GiHeadphones} />
-          <NavigationItem text={"Theatre Credits"} Icon={GiCheckboxTree} />
+          <NavigationItem text={"Theatre Credits"} Icon={GiMusicSpell} />
         </NavigationSubmenu>
-      }
+      </CSSTransition>
       <NavigationItem text={"Climate"} Icon={GiEarthAmerica} />
       <NavigationItem text={"Contact"} Icon={GiCandlestickPhone} />
     </nav>
