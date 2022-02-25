b8d5c4cf65e83035d1927365ff60c01d59e7d474
Travis Frank <travis.frank@placeexchange.com>
2022-02-24 23:36:20 -0500

    feat: Leadership (no project content)

--- src/components/about/Leadership.jsx
+++ src/components/about/Leadership.jsx
@@ -1,5 +1,6 @@
 import './Leadership.css';
 import BannerShell from '../shells/BannerShell';
+import SectionToggle from '../shells/SectionToggle';
 
 function Leadership() {
   return (
@@ -7,8 +8,21 @@ function Leadership() {
       bannerSrc="../../../src/assets/local/leadership.jpg"
       titleText="Leadership"
     >
-      <div className="leadership-content">
-      </div>
+      <p className="leadership-preamble">
+        In <i>Emerald City</i>, I wrote: "The greatest moments of all / are those
+        that makes us feel small". Every company and orchestra I found teaches
+        me that lesson again and again. Sorrounding myself with dedicated,
+        awe-inspiring people makes me feel small in the best way I can imagine.
+      </p>
+      <SectionToggle open={true} sectionTitle="Obsidian (2020-2021)">
+      </SectionToggle>
+      <SectionToggle
+        open={false}
+        sectionTitle="The Frank-Nilsson Contemporary Jazz Orchestra (2018-2019)"
+      >
+      </SectionToggle>
+      <SectionToggle open={false} sectionTitle="Black Tie Productions (2012-2016)">
+      </SectionToggle>
     </BannerShell>
   );
 }
--- src/components/about/Principles.jsx
+++ src/components/about/Principles.jsx
@@ -7,23 +7,21 @@ function Principles() {
       bannerSrc="../../../src/assets/local/principles.jpg"
       titleText="Principles"
     >
-      <div className="principles-content">
-        <p className="preamble">
-          Inspired by Derek Sivers's <a href="https://sive.rs/h"><i>How to Live</i></a> and
-          Ray Dalio's <a href="https://www.indiebound.org/book/9781501124020"><i>Principles</i></a>
-          , I publicly collect my principles here.
-        </p>
-        <p><b>Last Updated:</b> February 24, 2022</p>
-        <ul>
-          <li>Give gratitude</li>
-          <li>Embrace change</li>
-          <li>Walk uphill</li>
-          <li>Take responsibility</li>
-          <li>Practice awe</li>
-          <li>Shape reality</li>
-          <li>Stand out</li>
-        </ul>
-      </div>
+      <p className="principles-preamble">
+        Inspired by Derek Sivers's <a href="https://sive.rs/h"><i>How to Live</i></a> and
+        Ray Dalio's <a href="https://www.indiebound.org/book/9781501124020"><i>Principles</i></a>
+        , I publicly collect my principles here.
+      </p>
+      <p><b>Last Updated:</b> February 24, 2022</p>
+      <ul>
+        <li>Give gratitude</li>
+        <li>Embrace change</li>
+        <li>Walk uphill</li>
+        <li>Take responsibility</li>
+        <li>Practice awe</li>
+        <li>Shape reality</li>
+        <li>Stand out</li>
+      </ul>
     </BannerShell>
   );
 }
