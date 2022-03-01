3207cc902b162ed4f9cf1ad26f59b517714b0347
Travis Frank <travis.frank@placeexchange.com>
2022-03-01 18:55:02 -0500

    fix: Read values of postKeys

--- functions/api/writing/feed.js
+++ functions/api/writing/feed.js
@@ -2,8 +2,7 @@ export async function onRequestGet({ env }) {
   const respData = {};
 
   const postKeys = await env.travismfrank.get('post_keys');
-  console.log(await env.travismfrank.list());
-  for (const key in postKeys.split(',')) {
+  for (const key of postKeys.split(',')) {
     respData[key] = await env.travismfrank.get(key);
   }
 
