c0e35c0ef3b8b794f550a7c53fd28e0d8e800afe
Travis Frank <travis.frank@placeexchange.com>
2022-03-04 23:25:39 -0500

    feat: Musicals icons

--- src/components/music/Musicals.jsx
+++ src/components/music/Musicals.jsx
@@ -1,10 +1,55 @@
+import {
+  GiChefToque,
+  GiCyberEye,
+  GiGreekTemple,
+  GiMirrorMirror,
+  GiPrettyFangs,
+  GiSherlockHolmes,
+  GiSpaceNeedle,
+  GiTransfuse
+} from 'react-icons/gi';
 import { Link } from 'react-router-dom';
 
 import './Musicals.css';
 
 function Musicals() {
   return (
-    <div>
+    <div className="musicals-wrapper">
+      <div className="musical-selection-wrapper">
+        <h1>Original Musicals</h1>
+        <div className="musical-selection">
+          <GiTransfuse className="musical-icon" />
+          <h3>Reprise</h3>
+        </div>
+        <div className="musical-selection">
+          <GiChefToque className="musical-icon" />
+          <h3>Super Iron Cooking Chef</h3>
+        </div>
+        <div className="musical-selection">
+          <GiCyberEye className="musical-icon" />
+          <h3>2084</h3>
+        </div>
+        <div className="musical-selection">
+          <GiMirrorMirror className="musical-icon" />
+          <h3>Mirror, Mirror</h3>
+        </div>
+        <div className="musical-selection">
+          <GiSpaceNeedle className="musical-icon" />
+          <h3>Emerald City</h3>
+        </div>
+        <div className="musical-selection">
+          <GiSherlockHolmes className="musical-icon" />
+          <h3>Day One: A Sherlock Story</h3>
+        </div>
+        <div className="musical-selection">
+          <GiPrettyFangs className="musical-icon" />
+          <h3>Glitter: A Musical Parody</h3>
+        </div>
+        <div className="musical-selection">
+          <GiGreekTemple className="musical-icon" />
+          <h3>One-Acts</h3>
+        </div>
+      </div>
     </div>
   );
 }
