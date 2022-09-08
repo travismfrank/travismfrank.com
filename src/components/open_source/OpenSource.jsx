df2aee17bca3ab9789177a3bbd85db9845abd0fe
Travis Frank <travis@travismfrank.com>
2022-09-08 11:23:05 +0200

    feat: HyperDurable + Sponsor button in Open Source

--- src/components/open_source/OpenSource.jsx
+++ src/components/open_source/OpenSource.jsx
@@ -24,6 +24,13 @@ function OpenSource() {
       srcMap={openSourceSrcmap}
       titleText="Open Source"
     >
+      <SectionToggle open={true} sectionTitle="HyperDurable">
+        <p>
+          I am the builder and maintainer of <a href="https://github.com/ticket-bridge/hyper-durable" target="_blank">HyperDurable</a>,
+          an abstraction for <a href="https://developers.cloudflare.com/workers/runtime-apis/durable-objects/" target="_blank">Durable Objects</a> to
+          enable natural, object-like access to underlying persistent storage. Like RPC, for Durable Objects.
+        </p>
+      </SectionToggle>
       <SectionToggle open={true} sectionTitle="Obsidian">
         <p>
           I am a founding contributor of <a href="https://github.com/open-source-labs/obsidian" target="_blank">Obsidian</a>,
@@ -38,6 +45,12 @@ function OpenSource() {
           love PRs) or read <Link to={"/writing/2022-03-24_building-this-site"}>my post</Link> about the build.
         </p>
       </SectionToggle>
+      <div className="divider" />
+      <p>
+        If you want to support my open source work, sponsor me on GitHub by clicking the button below! I'm open to running workshops for your
+        team, joining your chat app, or even speaking at your conference. Let's make open source together.
+      </p>
+      <iframe id="sponsorButton" src="https://github.com/sponsors/TravisFrankMTG/button" title="Sponsor TravisFrankMTG" height="35" width="116" style={{border: "0"}}></iframe>
     </BannerShell>
   );
 }
